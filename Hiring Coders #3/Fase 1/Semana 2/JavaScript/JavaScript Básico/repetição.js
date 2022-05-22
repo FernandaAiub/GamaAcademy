dbf8b40bb11e3cb4dd874a9e697f10f3b9f8611e
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-21 21:31:33 -0300

    Exemplos de laço usando for em JS

---
+++ Hiring Coders #3/Fase 1/Semana 2/JavaScript/JavaScript Básico/repetição.js
@@ -0,0 +1,18 @@
+// Exemplo: número sorteado
+
+var numeroSorteado = 9
+
+for (var i = 0; i < 100; i++) {
+  if (numeroSorteado === i) {
+    console.log('Seu número foi encontrado!')
+    break
+  }
+}
+
+// Exemplo: tabuada
+
+var tabuada = 7
+
+for (var i = 0; i <= 10; i++) {
+  console.log('O valor de ' + tabuada + 'x' + i + ' = ' + tabuada * i)
+}
