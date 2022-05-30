02575818f96e2affd2ebf224ba12dae7b99d58e4
Fernanda Aiub <fernanda_aiub@hotmail.com>
2022-05-30 05:26:36 -0300

    Exemplo de uso de Template Strings

---
+++ Hiring Coders #3/Fase 1/Semana 2/JavaScript/JavaScript Intermediário/templatestring.js
@@ -0,0 +1,9 @@
+let nome = 'Fernanda'
+let sobrenome = 'Aiub'
+let profissao = 'Designer'
+
+console.log(
+  `Olá, meu nome é ${nome} ${sobrenome} e minha profissão é ${profissao}.`
+)
+
+console.log(`A soma de 1 + 1 é ${1 + 1}.`)
